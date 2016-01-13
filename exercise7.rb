3b28f729a5d86998d425e62313b48927e47d06eb
Filip Stankovic <filips@gmail.com>
2016-01-13 15:28:27 -0500

    Increase cohort size by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -15,3 +15,7 @@ display_students(students)
 
 #Output all cohort names
 puts students.keys
+
+#Increase size by 5%, rounded up
+students.each {|cohort, size| students[cohort] = (size * 1.05).round}
+display_students(students)
