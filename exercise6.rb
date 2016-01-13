8a91e3c75fe02ddc53f93259d8298672468408aa
Filip Stankovic <filips@gmail.com>
2016-01-13 14:19:13 -0500

    Counting items on list

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,10 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 def display_items(list)
   list.each {|item| puts "* #{item}"}
+  puts "Total items: #{list.length}"
 end
+
+# Add rice
+grocery_list << "rice"
 
 display_items(grocery_list)
